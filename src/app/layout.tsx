66f4493427dfde57c1d28677faa9cf4bbbe55550
Yuta123456 <yuuta09090530@icloud.com>
2023-04-02 20:01:09 +0900

    画像追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
 import Provider from "@/components/Provider";
 import StyleLayout from "@/components/StyleLayout";
 
-export const metadata = {
+import type { Metadata } from "next";
+export const metadata: Metadata = {
   title: "Dev Egg",
   description: "開発者の卵とデザイナーの卵のマッチングサービス",
+  openGraph: {
+    images: ["/DevEgg.png"],
+  },
+  icons: "/icon.png",
 };
 
 export default function RootLayout({

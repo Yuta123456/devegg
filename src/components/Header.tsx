00ff670ad2bd4962adcb79e6466f8932caaa9229
Yuta123456 <yuuta09090530@icloud.com>
2023-04-02 11:51:27 +0900

    logo追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,27 @@
 "use client";
 import { Box, Button, Container, Flex, Heading } from "@chakra-ui/react";
 import { FC } from "react";
-
+import Image from "next/image";
 export const Header: FC = () => {
   return (
     <Box h="100px" bg="white">
       <Container maxW="container.lg" h="100%">
         <Flex h="100%" alignItems="center" justifyContent="space-between">
-          <Heading fontSize="2xl">Dev Egg</Heading>
-          <Button size="lg" bg="yellow.100" _hover={{ bg: "yellow.200" }}>
+          <a href="/">
+            <Image
+              src="/DevEgg.svg"
+              alt="dev egg logo"
+              width={100}
+              height={100}
+              priority
+            />
+          </a>
+          <Button
+            size="lg"
+            bg="yellow.100"
+            _hover={{ bg: "yellow.200" }}
+            variant="outline"
+          >
             ログイン
           </Button>
         </Flex>

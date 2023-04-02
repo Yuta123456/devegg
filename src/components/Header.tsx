e906b6388f009c9188d616bb68f6edc3898151f6
Yuta123456 <yuuta09090530@icloud.com>
2023-04-02 14:43:06 +0900

    Headerのやつ

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,11 @@ import { Box, Button, Container, Flex, Heading } from "@chakra-ui/react";
 import { FC } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 export const Header: FC = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+  const isLoginPage = pathname === "/login";
   return (
     <Box h="100px" bg="white">
       <Container maxW="container.lg" h="100%">
@@ -22,6 +26,8 @@ export const Header: FC = () => {
             bg="yellow.100"
             _hover={{ bg: "yellow.200" }}
             variant="outline"
+            onClick={() => router.push("/login")}
+            hidden={isLoginPage}
           >
             ログイン
           </Button>
